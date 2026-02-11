668fdd9938ed97e26eb119fc5f80b6abae3ff86e
Kingpin2557 <lode.snaet@student.howest.be>
2026-02-11 13:49:55 +0100

    Add basic number addition example

---
+++ index.ts
@@ -0,0 +1,7 @@
+//basicTypes.runDemos();
+// functions.runDemos();
+// utilityTypes.runDemos();
+
+const aNumber: number = 5;
+const aSecondNumber: number = 6;
+const sum: number = aNumber + aSecondNumber;
